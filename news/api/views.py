8c49fce55a32cdc96138790bab5b1a3bee23633f
Amir Ayoub <techamir25@gmail.com>
2021-02-18 00:20:41 -0500

    init

--- news/api/views.py
+++ news/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response 
 
 from news.models import Article
